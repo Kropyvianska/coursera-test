2bc77e30486b781ab5d0a5bcf1f96f91b751f776
Mariia Kropyvianska <marykanarchy@gmail.com>
2019-07-14 23:27:59 +0200

    edit mistake

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -34,8 +34,8 @@ WARNING!!! WARNING!!!
 (function () {
 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    }
     
+
     // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
     // Loop over the names array and say either 'Hello' or "Good Bye"
     // using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -65,5 +65,3 @@ WARNING!!! WARNING!!!
     }
     
     })();
-
-    
